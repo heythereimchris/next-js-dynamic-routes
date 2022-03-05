37458e183722f70c978ca7ed42d3a61c56e671c6
CS <65098478+javascriptseo@users.noreply.github.com>
2022-03-04 20:40:33 -0800

    dynamic routes content

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <meta name="keywords" content="ninjas"/>
       </Head>
       <div>
-        <h1 className={styles.title}>Homepage</h1>
+        <h1 className={styles.title}>Next.js Dynamic Routes</h1>
         <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
         <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
         <Link href="/ninjas/">
